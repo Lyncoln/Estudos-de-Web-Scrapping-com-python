a03482423ea40f9b39927ee50da5c9698cf0f0ba
Lyncoln <lyncolnsousa@id.uff.br>
2019-02-08 04:15:34 -0200

    Atualização
    -Identifiquei um bug no site, que sempre repete 12 casas em todas as abas
    -Modifiquei meu spider para contornar esse problema
    -Agora consigo navegar em todas as casas do site

--- Scripts/mapaCultural.py
+++ Scripts/mapaCultural.py
@@ -1,5 +1,12 @@
 import scrapy
 
+#Há um problema com o site, quando você altera de página para acessar novas casas,
+#o site sempre irá mostrar as 12 primeiras casas, em qualquer que seja a aba
+#para isso dividi o código em 2 etapas, a primeira etapa ele irá pegar 
+#as 12 primeiras casas que sempre se repetem, e em seguida, irá executar um
+#loop para sempre pegar as 12 casas da parte de baixo do site
+
+
 
 class mapaCultura(scrapy.Spider):
     name = "cultura"
@@ -7,19 +14,29 @@ class mapaCultura(scrapy.Spider):
     def start_requests( self ):
         site = 'http://mapadecultura.rj.gov.br/categoria/espacos-culturais?page=1#ancora'
         yield scrapy.Request(url = site, callback = self.parse)
-    # Primeira etapa
+
+    #Primeira etapa consiste em pegar todos as casas que estão presentes na segunda caixa
+    #De apresentação do site
+    
+    
+    
     def parse(self, response):
-        links = response.css('h3 > a::attr(href)').extract()
+        segundoquadro = response.xpath('//*[@id="conteudo"]/div[5]/ul[2]//li/h3/a/@href').extract() 
         proxima = response.css('.proxima > a::attr(href)').extract_first()
-        for link in links :
+        for link in segundoquadro :
             yield response.follow(url = link, callback = self.parse2)
         if(proxima):
             yield response.follow(url = proxima, callback = self.parse)
-            
-            
-      
-        
-        
+        if(proxima is  None):
+            print("\n \n \n \n\n \n \n \n \n \n \n --------- \n \n \n \n \n \n \n \n \n \n \n \n \n")
+            yield response.follow(url = 'http://mapadecultura.rj.gov.br/categoria/espacos-culturais', callback= self.Comeco )
+    
+    #Por final, eu volto para primeira página e pego as 12 que sempre se repetem 
+    def Comeco(self, response) :
+        primeiroQuadro = response.xpath('//*[@id="conteudo"]/div[5]/ul[1]//li/h3/a/@href').extract()
+        for link in primeiroQuadro :
+            yield response.follow(url = link, callback = self.parse2)        
+     
         
     def parse2(self, response):
         print("\n")
